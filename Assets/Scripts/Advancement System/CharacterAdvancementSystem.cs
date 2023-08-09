e9de97948adf9d4305446290cd958b79f0d3ff0e
Christopher Cadwell <69337965+ChristopherCadwell@users.noreply.github.com>
2023-08-09 14:50:55 -0700

    Restore point

--- Assets/Scripts/Advancement System/CharacterAdvancementSystem.cs
+++ Assets/Scripts/Advancement System/CharacterAdvancementSystem.cs
@@ -8,22 +8,43 @@ public class CharacterAdvancementSystem : MonoBehaviour
 {
     private CharacterController characterController;
     private CharacterStats stats;
+    [SerializeField] private float strength;
+    [SerializeField] private float intelligence;
+    [SerializeField] private float willpower;
+    [SerializeField] private float luck;
+    [SerializeField] private float agility;
+    [SerializeField] private float charisma;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
         stats = GetComponent<CharacterStats>();
-
     }
 
     private void Update()
     {
-        // Check if the player is walking
         if (characterController.velocity.magnitude > stats.walkingThreshold)
         {
-            var agilityIncrease = 0.1f;
-            // Increase the agility progress vector
-            stats.IncreaseProgressVectors(0,0,agilityIncrease,0,0,0);
+            var agilityIncrease = 0.01f;
+            // Increase the Agility vector
+            agility = agility + agilityIncrease;
+
+            SetStats(strength, intelligence, willpower, luck, agility, charisma);
+            stats.UpdateValues();
+            Debug.Log("Updated");
         }
+    }
+    public void GetStats()
+    {
+        strength = stats.Stats.a;
+        intelligence = stats.Stats.b;
+        willpower = stats.Stats.c;
+        luck = stats.Stats.d;
+        agility = stats.Stats.e;
+        charisma = stats.Stats.f;
+    }
+    public void SetStats(float Str, float Int, float Wil, float Luc, float Agi, float Cha)
+    {
+        stats.Stats = new Vector6(Str, Int, Wil, Luc, Agi, Cha);
     }
 }
--- Assets/Scripts/Advancement System/CharacterStats.cs
+++ Assets/Scripts/Advancement System/CharacterStats.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using NVectors;
 using StarterAssets;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
@@ -17,76 +18,46 @@ public class CharacterStats : MonoBehaviour
     public float maxMana;
     public float baseMoveSpeed;
 
-    public float walkingThreshold = 1.0f;
+    public float walkingThreshold = 0.1f;
 
     public Vector6 Stats { get; set; }
-    public Vector6 ProgressVectors { get; private set; }
-
-    private Vector6 progressThresholds = new Vector6(1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
 
     private float thresholdMultiplier = 1.2f;
 
     private ThirdPersonController controller;
 
+    private CharacterAdvancementSystem system;
+
     private void Start()
     {
         controller = GetComponent<ThirdPersonController>();
+        system = GetComponent<CharacterAdvancementSystem>();
+
         // Assign the initial values of your stats to the Stats vector
         Stats = new Vector6(strength, intelligence, agility, willpower, luck, charisma);
-        ProgressVectors = new Vector6();
 
         // Determine misc values
         baseDefense = (strength + willpower) / 4;
         maxHealth = 20 + (strength * 2);
         maxMana = 20 + (intelligence * 2);
         baseMoveSpeed = 1 + (agility / 4);
+
+        system.GetStats();
     }
-
-    public void IncreaseAgility()
+    public void UpdateValues()
     {
-        // Define the increment for agility.
-        float agilityIncrement = 1f;
+        strength = Stats.a;
+        intelligence = Stats.b;
+        willpower = Stats.c;
+        luck = Stats.d;
+        agility = Stats.e;
+        charisma = Stats.f;
 
-        // Increase the agility stat
-        agility += agilityIncrement;
-
-        // Recalculate dependent values
-        baseMoveSpeed = 1 + (agility / 4);
-        controller.UpdateMoveSpeed(baseMoveSpeed);
-    }
-    private void CheckProgressVectors()
-    {
-        Debug.Log("Checking");
-
-        // Check if the agility progress vector has reached its threshold
-        if (ProgressVectors.c >= progressThresholds.c)
-        {
-            Debug.Log("True");
-
-            // Increase the agility stat
-            IncreaseAgility();
-
-            // Reset the agility progress vector
-            ProgressVectors = new Vector6(ProgressVectors.a, ProgressVectors.b, 0, ProgressVectors.d, ProgressVectors.e, ProgressVectors.f);
-
-            // Increase the agility progress vector threshold
-            progressThresholds = new Vector6(progressThresholds.a, progressThresholds.b, progressThresholds.c * thresholdMultiplier, progressThresholds.d, progressThresholds.e, progressThresholds.f);
-
-            // Update the Stats vector
-            Stats = new Vector6(strength, intelligence, agility, willpower, luck, charisma);
-        }
-
-
-        // Repeat for other stats...
-    }
-    public void IncreaseProgressVectors(float strength, float intelligence, float agility, float willpower, float luck, float charisma)
-    {
-        ProgressVectors = new Vector6(ProgressVectors.a + strength, ProgressVectors.b + intelligence, ProgressVectors.c + agility,
-        ProgressVectors.d + willpower, ProgressVectors.e + luck, ProgressVectors.f + charisma);
-        Debug.Log("Increased progress vectors " + ProgressVectors.c);
-
-        // Check if any progress vector has reached its threshold
-        CheckProgressVectors();
-
+        // Determine misc values
+        baseDefense = (Stats.a + Stats.d) / 4;
+        maxHealth = 20 + (Stats.a * 2);
+        maxMana = 20 + (Stats.b * 2);
+        baseMoveSpeed = 1 + (Stats.e / 4);
+        Debug.Log(baseMoveSpeed.ToString());
     }
 }
